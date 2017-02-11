20567afca40f70909f51f40010e58efdc93b0f06
Michael Heyer <Michael Heyer>
2017-02-11 10:22:33 +0100

    neuanlage projekt playground

---
+++ playground/src/org/feleni/playground/lambdas/RunJavaLambdas.java
@@ -0,0 +1,9 @@
+package org.feleni.playground.lambdas;
+
+public class RunJavaLambdas {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
---
+++ playground/src/org/feleni/playground/pdfbox/RunApachePdfBox.java
@@ -0,0 +1,9 @@
+package org.feleni.playground.pdfbox;
+
+public class RunApachePdfBox {
+
+	public static void main(String[] args) {
+
+	}
+
+}
