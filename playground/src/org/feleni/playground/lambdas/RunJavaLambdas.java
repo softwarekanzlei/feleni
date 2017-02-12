e8722dfcbe2ffe0bd4693dbb6bf0aa7aa2ffbf88
Michael Heyer <Michael Heyer>
2017-02-12 10:10:59 +0100

    refactor playground project

--- playground/src/org/feleni/playground/lambdas/RunJavaLambdas.java
+++
@@ -1,9 +0,0 @@
-package org.feleni.playground.lambdas;
-
-public class RunJavaLambdas {
-
-	public static void main(String[] args) {
-	
-	}
-
-}
--- playground/src/org/feleni/playground/pdfbox/RunApachePdfBox.java
+++
@@ -1,9 +0,0 @@
-package org.feleni.playground.pdfbox;
-
-public class RunApachePdfBox {
-
-	public static void main(String[] args) {
-
-	}
-
-}
